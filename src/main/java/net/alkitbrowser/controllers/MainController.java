3d1f601fdf158f9eed6e745f0be6501f9842edf9
juicyldoin <ldoinplay224@gmail.com>
2022-11-09 19:25:38 +0200

    added webEngine field

--- src/main/java/net/alkitbrowser/controllers/MainController.java
+++ src/main/java/net/alkitbrowser/controllers/MainController.java
@@ -3,6 +3,7 @@ package net.alkitbrowser.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import java.net.URL;
@@ -12,6 +13,7 @@ public class MainController implements Initializable {
 
     @FXML
     private WebView web;
+    private WebEngine webEngine;
 
     @FXML
     private HBox tabs;
@@ -19,7 +21,7 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
+        webEngine = web.getEngine();
 
     }
 }
