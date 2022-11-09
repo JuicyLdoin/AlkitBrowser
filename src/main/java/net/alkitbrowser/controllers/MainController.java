c1753fa9d4151a419b52ae6874ee2b3d8e14fefa
juicyldoin <ldoinplay224@gmail.com>
2022-11-09 19:18:12 +0200

    added main stage controller

---
+++ src/main/java/net/alkitbrowser/controllers/MainController.java
@@ -0,0 +1,25 @@
+package net.alkitbrowser.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private WebView web;
+
+    @FXML
+    private HBox tabs;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+    }
+}
