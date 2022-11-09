da5ff2574a9154b4136422a7c7de28cc0174312c
juicyldoin <ldoinplay224@gmail.com>
2022-11-09 21:21:20 +0200

    added page creation

--- src/main/java/net/alkitbrowser/controllers/MainController.java
+++ src/main/java/net/alkitbrowser/controllers/MainController.java
@@ -1,5 +1,7 @@
 package net.alkitbrowser.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
@@ -8,31 +10,50 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import net.alkitbrowser.page.Page;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class MainController implements Initializable {
 
     @FXML
-    private WebView web;
-    private WebEngine webEngine;
+    WebView web;
+    WebEngine webEngine;
 
-    // поле окон
+    // список окон
+    final ObservableList<Page> pages = FXCollections.observableArrayList();
     @FXML
     private HBox tabs;
 
     // поле в котором пользователь будет вводить ссылку или запрос
     @FXML
-    private TextField requestField;
+    TextField requestField;
+
+    public void addPage() {
+
+        Page page = new Page();
+        page.createNewPage(webEngine, "");
+
+        pages.add(page);
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         webEngine = web.getEngine();
 
+        pages.addListener((javafx.beans.Observable observable) -> {
+
+            tabs.getChildren().clear();
+            pages.forEach(page -> tabs.getChildren().add(page.getPageController().getBody()));
+
+        });
     }
 
     public void updateRequest() {
@@ -64,6 +85,13 @@ public class MainController implements Initializable {
 
         if (keyEvent.getCode().equals(KeyCode.ENTER))
             updateRequest();
+
+    }
+
+    @FXML
+    private void onAddPage() {
+
+        addPage();
 
     }
 }
