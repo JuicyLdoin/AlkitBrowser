e4694c52ee47d9435fe1fd803a68bdc91284cd93
juicyldoin <ldoinplay224@gmail.com>
2022-11-09 19:37:16 +0200

    update controller (added button listener and url`s field)

--- src/main/java/net/alkitbrowser/controllers/MainController.java
+++ src/main/java/net/alkitbrowser/controllers/MainController.java
@@ -2,6 +2,7 @@ package net.alkitbrowser.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -15,13 +16,29 @@ public class MainController implements Initializable {
     private WebView web;
     private WebEngine webEngine;
 
+    // поле окон
     @FXML
     private HBox tabs;
+
+    // поле в котором пользователь будет вводить ссылку или запрос
+    @FXML
+    private TextField requestField;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         webEngine = web.getEngine();
+
+    }
+
+    // обработка клика по кнопке поиска
+    @FXML
+    private void onFindClick() {
+
+        String text = requestField.getText();
+
+        if (!text.equals(""))
+            webEngine.load(text);
 
     }
 }
