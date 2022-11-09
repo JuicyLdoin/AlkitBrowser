d205e1be99a5a99355bdab61ccb0c5819d905b35
juicyldoin <ldoinplay224@gmail.com>
2022-11-09 20:04:35 +0200

    added tab

---
+++ src/main/java/net/alkitbrowser/controllers/TabController.java
@@ -0,0 +1,15 @@
+package net.alkitbrowser.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TabController {
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private void onCloseTab() {
+
+    }
+}
