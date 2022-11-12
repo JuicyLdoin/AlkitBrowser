992fd4e57b74db13f4f52e66a54963e0fa43c731
juicyldoin <ldoinplay224@gmail.com>
2022-11-12 13:14:21 +0200

    bug fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.alkitbrowser {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.graphics;
 
     requires com.google.gson;
 
@@ -13,6 +14,6 @@ module com.example.alkitbrowser {
 
     opens net.alkitbrowser.controllers to javafx.fxml, javafx.web, lombok, com.google.gson;
     opens net.alkitbrowser.page to javafx.fxml, javafx.web, lombok, com.google.gson;
-    opens net.alkitbrowser to javafx.fxml, javafx.web, lombok, com.google.gson;
+    opens net.alkitbrowser to javafx.fxml, javafx.graphics, javafx.web, lombok, com.google.gson;
 
 }
